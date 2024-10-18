292cd07bde02ceb4226e16972dfa661a263b2d71
Julian <cjk@rabidaudio.com>
2024-10-18 17:34:53 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    liveReload: true,
+    compress: true,
+    port: 8000,
+  },
+};
